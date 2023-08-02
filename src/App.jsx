50d6ac0a3ae5c43d1a1dca9c66ce383aae59e173
MALWICK <batahumphrey66@gamil.com>
2023-08-02 12:49:20 +0100

    create routes and use navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Food from './pages/Food/Food';
+import FoodDescription from './pages/RecipeDescription/RecipeDescription';
 import { DataProvider } from './constext/DataContext';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div>
       <DataProvider>
-        <Food />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="food" element={<Food />} />
+            <Route path="fooddescription" element={<FoodDescription />} />
+          </Routes>
+        </BrowserRouter>
       </DataProvider>
     </div>
   );
