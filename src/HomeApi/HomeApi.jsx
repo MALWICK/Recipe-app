1fc933396910bb61a1be6a79341175b7f7d7cf8a
MALWICK <batahumphrey66@gamil.com>
2023-09-08 03:00:10 +0100

    imported folder

---
+++ src/HomeApi/HomeApi.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Vegge from './Popular.jsx/Vegge/Vegge';
+import Popular from './Popular.jsx/Popular';
+
+function HomeApi() {
+  return (
+    <div>
+      <Vegge />
+      <Popular />
+    </div>
+  );
+}
+
+export default HomeApi;
