fedf8e4d2ca22f33968d3e9d9a8e23bbd2009aa9
MALWICK <batahumphrey66@gamil.com>
2023-09-12 09:21:04 +0100

    succeded fetching from Api

--- src/HomeApi/Popular.jsx/Popular.jsx
+++ src/HomeApi/Popular.jsx/Popular.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Popular() {
-  return <div>Popular</div>;
+function App() {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://www.themealdb.com/api/json/v1/1/search.php?f=a'
+        );
+        const data = await response.json();
+        setMeals(data.meals);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Meals</h1>
+      <ul>
+        {meals.map((meal) => (
+          <li key={meal.idMeal}>{meal.strMeal}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Popular;
+export default App;
