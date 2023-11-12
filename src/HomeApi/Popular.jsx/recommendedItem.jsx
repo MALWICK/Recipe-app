e381fa378fc1f4b048c148cba8faa8de0a9956de
MALWICK <batahumphrey66@gamil.com>
2023-11-12 20:48:31 +0100

    trying to set a timeout on my fectch data

--- src/HomeApi/Popular.jsx/recommendedItem.jsx
+++ src/HomeApi/Popular.jsx/recommendedItem.jsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Styles from './recommendeditem.module.css';
 
+const fetchedData = () => {
+  const controller = new AbortController();
+  const { signal } = controller;
+};
+
 function App() {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,34 +17,45 @@ function App() {
           'https://www.themealdb.com/api/json/v1/1/random.php'
         );
         const data = await response.json();
+
         setMeals(data.meals);
+        setIsLoading(false);
       } catch (error) {
         console.error('Error:', error);
+        setIsLoading(false);
       }
     };
 
     fetchData();
   }, []);
 
-  /*  console.log(meals); */
+  if (isLoading) {
+    return (
+      <div className={Styles.loader}>
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
 
   return (
     <div>
-      {meals.map((meal) => (
-        <div className={Styles.recom}>
-          <div className={Styles.recom__inner}>
-            <div className={Styles.overlay} />
-            <h1 className={Styles.topleft} key={meal.idMeal}>
-              {meal.strMeal}
-            </h1>
-            <img
-              src={meal.strMealThumb}
-              alt="meal"
-              className={Styles.imgmeal}
-            />
+      {meals.length > 0 ? (
+        meals.map((meal) => (
+          <div className={Styles.recom} key={meal.idMeal}>
+            <div className={Styles.recom__inner}>
+              <div className={Styles.overlay} />
+              <h1 className={Styles.topleft}>{meal.strMeal}</h1>
+              <img
+                src={meal.strMealThumb}
+                alt="meal"
+                className={Styles.imgmeal}
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <div>No meals found.</div>
+      )}
     </div>
   );
 }
