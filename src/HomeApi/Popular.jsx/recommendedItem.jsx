80edf7d18aeffc116a9d64c1206a0096f651b050
MALWICK <batahumphrey66@gamil.com>
2023-11-12 21:15:03 +0100

    updating loadout

--- src/HomeApi/Popular.jsx/recommendedItem.jsx
+++ src/HomeApi/Popular.jsx/recommendedItem.jsx
@@ -1,32 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import Styles from './recommendeditem.module.css';
 
-const fetchedData = () => {
+const fetchData = async () => {
   const controller = new AbortController();
   const { signal } = controller;
+
+  try {
+    const response = await fetch(
+      'https://www.themealdb.com/api/json/v1/1/random.php',
+      { signal }
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  } finally {
+    controller.abort(); // Cancel request if it's still pending
+  }
 };
 
 function App() {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [errors, setErrors] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          'https://www.themealdb.com/api/json/v1/1/random.php'
-        );
-        const data = await response.json();
+    const timeout = setTimeout(() => {
+      fetchData()
+        .then((fetchedData) => {
+          setMeals(fetchedData.meals);
+          setIsLoading(false); // Set isLoading to false after fetching data
+        })
+        .catch((error) => {
+          setErrors(error);
+          setIsLoading(false); // Set isLoading to false on error
+        });
+    }, 8000);
 
-        setMeals(data.meals);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error:', error);
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
+    return () => clearTimeout(timeout); // Clear timeout on component unmount
   }, []);
 
   if (isLoading) {
@@ -39,6 +51,7 @@ function App() {
 
   return (
     <div>
+      {errors && <div className="error">{errors.message}</div>}
       {meals.length > 0 ? (
         meals.map((meal) => (
           <div className={Styles.recom} key={meal.idMeal}>
