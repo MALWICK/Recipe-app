dd80280de9a26848df563d4909e902c8a1c0dd57
MALWICK <batahumphrey66@gamil.com>
2023-11-19 21:14:46 +0100

    updating navbar

--- src/HomeApi/Vegge/Vegge.jsx
+++ src/HomeApi/Vegge/Vegge.jsx
@@ -1,39 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './vegge.css';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
 function Vegge() {
-  const navigate = useNavigate();
-  const [mainData, setMainData] = useState([]);
-  console.log(mainData, 'jakas');
-
-  useEffect(() => {
-    axios
-      .get('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then((response) => setMainData(response.data.meals))
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  const handleMealClick = (meal) => {
-    navigate(`/meal-description/${meal.idMeal}`);
-  };
-
   return (
     <div className="vegge">
-      {mainData?.map((meal) => (
-        <div
-          className="vegge__container"
-          key={meal.idMeal}
-          style={{ backgroundImage: `url(${meal.strMealThumb})` }}
-          onClick={() => handleMealClick(meal)}
-          aria-hidden="true"
-        >
-          <p>ff</p>
-        </div>
-      ))}
+      <h1>Der Gricht</h1>
     </div>
   );
 }
