6cd88c1f92ef6ad0b789d3501dad3c5e72927e5f
MALWICK <batahumphrey66@gamil.com>
2023-11-25 13:05:20 +0100

    succeded in fetching from api

--- src/HomeApi/category/category.jsx
+++ src/HomeApi/category/category.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function catergory() {
-  return <div>d</div>;
+  const [categories, setCatogories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
+      .then((response) => response.json())
+      .then((data) => setCatogories(data.meals))
+      .catch((error) => console.log(error));
+  }, [2]);
+  console.log(categories);
+  return (
+    <div className="categories-container">
+      <p>changam</p>
+    </div>
+  );
 }
 
 export default catergory;
