90b40e99c399c8098707893e83cf95832add80b5
MALWICK <batahumphrey66@gamil.com>
2023-11-29 16:31:59 +0100

    create mcategory descriptiom routes Dynamiclly

--- src/pages/catergoryDescription/categoryDescription.jsx
+++ src/pages/catergoryDescription/categoryDescription.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function categoryDescription() {
+  const { categoryName } = useParams();
+  const [dishes, setDishes] = useState([]);
+  console.lo(dishes);
+
+  useEffect(() => {
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoryName}`
+    )
+      .then((response) => response.json())
+      .then((data) => setDishes(data.meals))
+      .catch((error) => console.log(error));
+  }, [categoryName]);
   return (
     <div>
       <p>iudsiuiusdiuuduide</p>
