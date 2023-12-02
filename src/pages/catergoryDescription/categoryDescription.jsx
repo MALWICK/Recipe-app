2406e948d7950713ff70feba6ce7c25e3e041e2e
MALWICK <batahumphrey66@gamil.com>
2023-12-02 22:10:47 +0100

    add loader to the fectch

--- src/pages/catergoryDescription/categoryDescription.jsx
+++ src/pages/catergoryDescription/categoryDescription.jsx
@@ -5,6 +5,7 @@ import axios from 'axios';
 function CategoryDescription() {
   const { category } = useParams();
   const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -13,20 +14,29 @@ function CategoryDescription() {
           `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
         );
         setMeals(response.data?.meals);
+        setLoading(false);
       } catch (error) {
         console.log(error);
+        setLoading(false);
       }
     };
 
     fetchData();
   }, [category]);
+  if (loading) {
+    return <div>Loading...</div>; // Render a loader while fetching data
+  }
 
+  console.log(meals);
   return (
     <div>
       <h2>Category: {category}</h2>
       <ul>
         {meals.map((meal) => (
-          <li key={meal.idMeal}>{meal.strMeal}</li>
+          <li key={meal.idMeal}>
+            <img src={meal.strMealThumb} alt="meal" />
+            {meal.strMeal}
+          </li>
         ))}
       </ul>
     </div>
