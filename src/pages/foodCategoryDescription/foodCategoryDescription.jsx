67fe408328b3eedab1abe09de430fb1b32cb343b
MALWICK <batahumphrey66@gamil.com>
2023-12-03 23:55:46 +0100

    updating my code

--- src/pages/foodCategoryDescription/foodCategoryDescription.jsx
+++ src/pages/foodCategoryDescription/foodCategoryDescription.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
+import ReactPlayer from 'react-player';
 
 function foodCategoryDescription() {
   const { mealId } = useParams();
@@ -20,24 +21,60 @@ function foodCategoryDescription() {
 
     fetchMeal();
   }, [mealId]);
+  console.log(meal);
 
   if (!meal) {
     return <div>Loading meal...</div>;
   }
 
+  const ingredients = Object.entries(meal)
+    .filter(([key, value]) => key.startsWith('strIngredient') && value)
+    .map(([key, value]) => {
+      const measureKey = `strMeasure${key.slice(13)}`;
+      const measure = meal[measureKey];
+      return {
+        ingredient: value,
+        measure,
+      };
+    });
+
   return (
-    <div>
-      <h2>Meal: {meal.strMeal}</h2>
-      <img src={meal.strMealThumb} alt={meal.strMeal} />
-      <h3>Ingredients:</h3>
-      <ul>
-        {/* Render the list of ingredients */}
-        {Object.keys(meal)
-          .filter((key) => key.includes('strIngredient') && meal[key])
-          .map((key) => (
-            <li key={key}>{meal[key]}</li>
-          ))}
-      </ul>
+    <div className="mealsdescription">
+      <div className="mealDescription__container">
+        <div className="back__container">
+          <button type="button" className="back__btn">
+            <i className="fa-solid fa-arrow-left" />
+            Back
+          </button>
+        </div>
+        <div className="meal__info">
+          <img src={meal.strMealThumb} alt="meal" />
+
+          <div className="fooddetails-container">
+            <h1>{meal.strMeal}</h1>
+            <span className="food__origin">
+              Origin: <span className="bluetext">{meal.strArea}</span>
+            </span>
+            <h2 className="ingre">Ingredients</h2>
+            <ul className="lister">
+              {ingredients.map(({ ingredient, measure }) => (
+                <li key={measure}>
+                  {ingredient} - <span className="lighttext">{measure}</span>
+                </li>
+              ))}
+            </ul>
+            <p>{meal.strInstructions}</p>
+            <div className="player-wrapper">
+              <ReactPlayer
+                url={meal.strYoutube}
+                className="react-player"
+                width="100%"
+                height="100%"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
