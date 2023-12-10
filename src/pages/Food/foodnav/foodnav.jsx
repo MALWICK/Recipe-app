11df783c12157a9744de8ea9a256b5a585710959
MALWICK <batahumphrey66@gamil.com>
2023-12-10 13:11:48 +0100

    update the food navbar

---
+++ src/pages/Food/foodnav/foodnav.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function foodnav() {
+  return (
+    <div className="backlinks">
+      <div className="backlins-container">
+        <div className="fisrt">
+          <div className="thehedding">Der Gericht</div>
+        </div>
+        <div className="second">
+          <div className="backhome">
+            <button type="button">Back</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default foodnav;
